f77106160dce509dde16271cf8dc5fe9610b41b1
vinicius <viniciusbmarco10@gmail.com>
2022-03-16 19:25:26 -0300

    atualizacao

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 comando para executar o script
 ```
-# pacman -S git
-git clone https://github.com/auzne/instalador-arch.git
-cd instalador-arch
-chmod +x ./instalador-arch.sh
-./instalador-arch.sh
+# yes | pacman -Sy git
+# git clone https://github.com/auzne/instalador-arch.git
+# cd instalador-arch
+# chmod +x ./instalador-arch.sh
+# ./instalador-arch.sh
 ```
--- instalador-arch.sh
+++ instalador-arch.sh
@@ -2,13 +2,15 @@
 # nao suporte uefi
 # somente conexão cabeada
 
-
 # carrega o teclado br
-loakeys br-abnt2
+echo "Carregando mapeamento de teclado BR ABNT2"
+loadkeys br-abnt2
 
+# locale br
+echo "Gerando o Locale pt-br"
+sed -i 's/en_US.UTF-8 UTF-8/#en_US.UTF-8 UTF-8/g' /etc/locale.gen
 echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
 timedatectl set-ntp true
-
 
